a4a0e6896feb7d3424d69c66a920f09fb6ef6730
Paolo Borello <142054184+paoloborello@users.noreply.github.com>
2024-01-24 07:45:50 -0500

    Update ex3.py - Finished
    Completed ex3 by finishing implementing Algorithm 17.1 of ESL

--- HW 1/code/ex3.py
+++ HW 1/code/ex3.py
@@ -11,11 +11,13 @@ def pad_vector(vec: np.array, pos: list, val=0) -> np.array:
     return v_padded
 
 
-def mra_euggm_ks(S: np.array, not_E: list, max_iter=1000, tol=1e-6) -> np.array:
-    # initialize W
+def mra_euggm_ks(S: np.array, not_Edges: list, max_iter=1000, tol=1e-6) -> np.array:
+    # initialize W and Theta
     W = S
+    T = np.zeros_like(W)
     # get number of nodes
     p = np.shape(S)[0]
+    conv_flag = False
     for n_iter in range(max_iter):
         W_old = np.copy(W)
         for j in range(p):
@@ -23,9 +25,8 @@ def mra_euggm_ks(S: np.array, not_E: list, max_iter=1000, tol=1e-6) -> np.array:
             mask = np.ones(p, dtype=bool)
             # slice out row and column j
             mask[j] = False
-            W_11 = W[mask, :][:, mask]
             # slice out edges not incident to j
-            for edge in not_E:
+            for edge in not_Edges:
                 if edge[0] == j:
                     mask[edge[1]] = False
                 elif edge[1] == j:
@@ -35,45 +36,45 @@ def mra_euggm_ks(S: np.array, not_E: list, max_iter=1000, tol=1e-6) -> np.array:
             s_star = S[mask, j]
             # solve for beta_star
             beta_star = np.linalg.solve(W_star, s_star)
-            # correct up to this point
-
-            # list of nodes not incident to j
-            # mask[j] = True
+            # list of nodes not incident to j (and coordinate j)
             not_j = np.arange(p)[np.invert(mask)]
             # pad beta_star with zeros at appropriate positions
             beta = pad_vector(beta_star, not_j, 0)
-            print(f"""{"-" * 40}""")
-            print(j, not_j)
-            print(beta)
+            # compute w (similar to w12 but element j is wrong)
             w = np.matmul(W, beta)
+            # set element j to correct value
             w[j] = W[j, j]
-
-            # w_12 = np.matmul(W_11, beta)
-            # w_12 = pad_vector(w_12, [j], W[j, j])
-            print(f"""{"__" * 40}""")
-            print(f"""w: {w}""")
-            print(f"""W before up: {W}""")
+            # set j-th row and column of W to w
             W[j] = w
             W[:, j] = w
-            print(f"""W after up: {W}""")
+            # if we converged, compute Theta
+            if conv_flag:
+                theta_22 = 1 / (S[j, j] - np.dot(w, beta))
+                theta_12 = - beta * theta_22
+                theta_12[j] = theta_22
+                T[j] = theta_12
 
-
-        # check convergence
-        if np.linalg.norm(W - W_old) < tol:
+        # if converged break
+        if conv_flag:
             break
+        # check convergence
+        norm = np.linalg.norm(W - W_old)
+        if norm < tol:
+            conv_flag = True
+        print(f"Iter {n_iter + 1} - Norm: {round(norm, 7)}")
 
-    print(f"""final iter: {n_iter}""")
-    return W
+    return W, T
 
 
-def main():
+if __name__ == '__main__':
     # ESL 17.4 example
+
     # sample covariance matrix
     cov_S = np.array([[10, 1, 5, 4], [1, 10, 2, 6], [5, 2, 10, 3], [4, 6, 3, 10]], dtype=float)
     # list of missing edges
     not_E = [(0, 2), (1, 3)]
-    A = mra_euggm_ks(cov_S, not_E)
-
-
-if __name__ == '__main__':
-    main()
+    Sigma, Theta = mra_euggm_ks(cov_S, not_E)
+    print("W: ")
+    print(np.round(Sigma, decimals=3))
+    print("W_inv: ")
+    print(np.round(Theta, decimals=3))
